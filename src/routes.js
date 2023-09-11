e2cbac68f534d0f9ffa8b1014567fd237b3e0693
Cléber Severo <clebersevero1998@gmail.com>
2023-09-11 17:45:38 -0300

    feat: standard page component for routing

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
 import Menu from 'components/Menu';
+import PaginaPadrao from 'components/Menu/PaginaPadrao';
 
 export default function AppRouter() {
   return (
     <Router>
       <Menu />
       <Routes>
-        <Route path='/' element={<Inicio />}/>
-        <Route path='/cardapio' element={<Cardapio />} />
+        <Route path='/' element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path='cardapio' element={<Cardapio />} />
+        </Route>
       </Routes>
     </Router>
   );
