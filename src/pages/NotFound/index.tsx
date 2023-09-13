f0e3560e8c1fe7247325f23291cba8e17ee26629
Cléber Severo <clebersevero1998@gmail.com>
2023-09-12 22:41:44 -0300

    feat: not found page created

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './NotFound.module.scss';
+import {ReactComponent as NotFoundPage} from 'assets/not_found.svg' ;
+import classNames from 'classnames';
+import stylesTema from 'styles/Tema.module.scss';
+
+export default function NotFound() {
+  return (
+    <div className={classNames({
+      [styles.container]: true,
+      [stylesTema.container]: true
+    })}>
+      <NotFoundPage />
+    </div>
+  );
+}
